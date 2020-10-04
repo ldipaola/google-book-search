77edde57b12a3341ca98ea9c279c3a3aefdbbd91
Lukey <ldipaola66@gmail.com>
2020-10-04 21:30:15 +1100

    init project and create server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(require("./routes/api-routes.js"));
+
+
+app.listen(PORT, () => `Server running on port ${PORT}`);
