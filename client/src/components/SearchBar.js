f97b590a2f134053fb075f90bcdc079bb8cde093
Lukey <ldipaola66@gmail.com>
2020-10-05 23:28:07 +1100

    create compenent

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, {useState, useContext} from "react";
+import axios from "axios";
+import BooksContext from "../utils/booksContext";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
+  const { books , setBooks } = useContext(BooksContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const bookSearch = await axios.get("https://www.googleapis.com/books/v1/volumes?q="+search.trim())
+    setBooks(bookSearch);
+    console.log(bookSearch.data.items[0]);
+  }
+
+  return (
+    <div className="container" style={{marginTop:"8px"}}>
+      <div className="card">
+      <div className="card-header">
+        <div className="card-title h5">Book Search</div>
+        <div className="card-subtitle text-gray">Book</div>
+      </div>
+      <div className="card-body">
+      <div className="input-group ">
+          <input className="form-input" type="text" placeholder="search" onChange={(e) => setSearch(e.target.value)} />
+          <button className="btn btn-primary input-group-btn" onClick={handleSubmit}>Search</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
