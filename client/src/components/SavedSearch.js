485e22b408e876cdbc948b37c26249b87cc8f515
Lukey <ldipaola66@gmail.com>
2020-10-07 23:48:08 +1100

    create component for saving searches

---
+++ client/src/components/SavedSearch.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import api from "../utils/api";
+import BookCard from "../components/BookCard";
+
+export default function SavedSearch() {
+  const [savedBooks, setSavedBooks] = useState();
+  const [deletedBook, setDeletedBook] = useState();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget.dataset.value);
+    api.deleteBook(e.currentTarget.dataset.value).then((data) => {
+      setDeletedBook(data.data);
+    });
+  };
+
+  useEffect(() => {
+    api
+      .getBooks()
+      .then((data) => {
+        setSavedBooks(
+          data.data.map((book) => (
+            <BookCard
+              key={book._id}
+              id={book._id}
+              title={book.title}
+              author={book.author}
+              description={book.description}
+              image={book.image}
+              link={book.link}
+              onClick={handleClick}
+            />
+          ))
+        );
+      })
+      .catch((err) => console.log(err));
+  }, [deletedBook]);
+
+  return <div>{savedBooks}</div>;
+}
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SavedSearch from "../components/SavedSearch";
 
 export default function Saved() {
   return (
     <div>
-      <h1>Saved books</h1>
+      <SavedSearch />
     </div>
   );
 }
