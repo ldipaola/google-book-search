fe6956f4b9327a04e6e6784a30babfd92eeb6498
Lukey <ldipaola66@gmail.com>
2020-10-08 13:05:00 +1100

    build

---
+++ client/build/precache-manifest.c7b358e9599bc197f7206e137e3752d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e06b315a46ce9c1ce6f1949b760caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24df6d759b453179ed00",
+    "url": "/static/css/2.01d1bec1.chunk.css"
+  },
+  {
+    "revision": "5718f1becbb5e6ef03cc",
+    "url": "/static/css/main.ebb9e487.chunk.css"
+  },
+  {
+    "revision": "24df6d759b453179ed00",
+    "url": "/static/js/2.50b24301.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50b24301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5718f1becbb5e6ef03cc",
+    "url": "/static/js/main.18e6b18b.chunk.js"
+  },
+  {
+    "revision": "bd28a860165ae406def9",
+    "url": "/static/js/runtime-main.561f0976.js"
+  }
+]);
